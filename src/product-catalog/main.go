a6a00c060aa8db64de231a2d3e137e454848e1f2
SAI <saiprasanna2626@gmail.com>
2025-03-01 23:09:24 -0800

    chore: verify github actions

--- src/product-catalog/main.go
+++ src/product-catalog/main.go
@@ -314,6 +314,11 @@ func createClient(ctx context.Context, svcAddr string) (*grpc.ClientConn, error)
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
 	)
+
+
+
+
+	
 }
 
 
